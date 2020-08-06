72ab6f30b5c0d2537f9635a178b3a893ddd9ee7b
nandabalassoni <mariafernandabalassoni@hotmail.com>
2020-08-06 01:12:16 -0300

    adicionando dados para listagem de professores

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles.css'
 import PageHeader from '../../components/PageHeader';
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 function TeacherList(){
     return(
@@ -24,7 +25,35 @@ function TeacherList(){
                     </div>
 
                 </form> 
-            </PageHeader>     
+            </PageHeader>
+
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="https://avatars1.githubusercontent.com/u/33336355?s=460&u=24da94177f2e62cb27fff42f57aa49395497a423&v=4" alt="Maria Fernanda"/>
+                        <div>
+                            <strong> Maria Fernanda</strong>
+                            <span>Química</span>
+                        </div>
+                    </header>
+
+                    <p> Entusiasta das melhores tecnologias de química avançada. 
+                        <br /> <br />
+                        Apaixonada por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+                    </p>
+
+                    <footer>
+                        <p>
+                            Preço/hora:
+                            <strong>R$ 80,00</strong>
+                        </p>
+                        <button type="button">
+                            <img src={whatsappIcon} alt="Icone WhatsApp"/>
+                            Entrar em contato
+                        </button>
+                    </footer>
+                </article>
+            </main>     
         </div>
     )
 }
