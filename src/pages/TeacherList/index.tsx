e8205a719af9f299621e693332348c89fb619273
nandabalassoni <mariafernandabalassoni@hotmail.com>
2020-08-06 01:45:07 -0300

    criando a estrutura html do componente

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import './styles.css'
+import './styles.css';
 import PageHeader from '../../components/PageHeader';
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import TeacherItem from '../../components/TeacherItem';
+
 
 function TeacherList(){
     return(
@@ -28,31 +29,16 @@ function TeacherList(){
             </PageHeader>
 
             <main>
-                <article className="teacher-item">
-                    <header>
-                        <img src="https://avatars1.githubusercontent.com/u/33336355?s=460&u=24da94177f2e62cb27fff42f57aa49395497a423&v=4" alt="Maria Fernanda"/>
-                        <div>
-                            <strong> Maria Fernanda</strong>
-                            <span>Química</span>
-                        </div>
-                    </header>
+                <TeacherItem />
+                    
+                <TeacherItem />
 
-                    <p> Entusiasta das melhores tecnologias de química avançada. 
-                        <br /> <br />
-                        Apaixonada por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-                    </p>
+                <TeacherItem />
 
-                    <footer>
-                        <p>
-                            Preço/hora:
-                            <strong>R$ 80,00</strong>
-                        </p>
-                        <button type="button">
-                            <img src={whatsappIcon} alt="Icone WhatsApp"/>
-                            Entrar em contato
-                        </button>
-                    </footer>
-                </article>
+                <TeacherItem />
+
+                <TeacherItem />
+
             </main>     
         </div>
     )
