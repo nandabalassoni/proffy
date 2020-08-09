a6262499f4e5e68a25cf977b930c492ca70fc096
nandabalassoni <mariafernandabalassoni@hotmail.com>
2020-08-08 23:01:55 -0300

    aplicando o component Input no arquivo Index para o formulario de cadastro de professores

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import './styles.css'
+import Input from '../../components/Input';
+import warningIcon from '../../assets/images/icons/warning.svg'
+
+
 
 function TeacherForm(){
     return(
@@ -8,27 +12,36 @@ function TeacherForm(){
             <PageHeader title="Que incrível que você quer dar aulas."
             description="O primeiro passo é preencher esse formulário de inscrição"
             />
-            
+
             <main>
                 <fieldset>
                     <legend> Seus dados </legend>
 
-                    <div className="input-block">
-                        <label htmlFor="name"> Nome completo</label>
-                        <input type="text" id="name"/>
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="avatar"> Avatar</label>
-                        <input type="text" id="avatar"/>
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="whatsapp"> WhatsApp  </label>
-                        <input type="text" id="whatsapp"/>
-                    </div>
+                    <Input name="name" label="Nome completo" />
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsapp" label="WhatsApp" />
 
                 </fieldset>
+
+                <fieldset>
+                    <legend> Sobre a aula </legend>
+
+                    <Input name="subject" label="Matéria" />
+                    <Input name="cost" label="Custo da sua hora por aula" />
+
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso importante"/>
+                        Importante! <br />
+                        Preencha todos os dados
+                    </p>
+
+                    <button type="button">
+                        Salvar cadastro
+                    </button>
+                </footer>
             </main>     
         </div>
     )
