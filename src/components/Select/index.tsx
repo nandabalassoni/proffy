e932f5efceea9a2d9c6d805068b7df3f62f98306
nandabalassoni <mariafernandabalassoni@hotmail.com>
2020-08-09 03:11:20 -0300

    atualizando arquivo

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -16,8 +16,9 @@ const Select: React.FunctionComponent<SelectProps> = ({ label, name, options, ..
     return(
         <div className="select-block">
             <label htmlFor={name}>{label}</label>
-            <select id={name} {...rest}>
-                <option value="" disabled selected hidden> Selecione uma matéria</option>
+            <select defaultValue="" id={name} {...rest}>
+                <option value="" disabled hidden> Selecione uma opção</option>
+
                 {options.map(option =>{
                     return <option key={option.value} value={option.value}>{option.label}</option>
                 })} 
