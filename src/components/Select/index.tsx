a2d01e45116cbe2a1317739d99d4d3ce4b4bfcf5
nandabalassoni <mariafernandabalassoni@hotmail.com>
2020-08-09 00:26:59 -0300

    adicionando opção default para o select

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -17,8 +17,9 @@ const Select: React.FunctionComponent<SelectProps> = ({ label, name, options, ..
         <div className="select-block">
             <label htmlFor={name}>{label}</label>
             <select id={name} {...rest}>
+                <option value="" disabled selected hidden> Selecione uma matéria</option>
                 {options.map(option =>{
-                    return <option value={option.value}>{option.label}</option>
+                    return <option key={option.value} value={option.value}>{option.label}</option>
                 })} 
             </select>
         </div>
