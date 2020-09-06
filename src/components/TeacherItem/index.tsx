0d6d137c50bf2610dfab650c7a82a1d70d7d5213
nandabalassoni <mariafernandabalassoni@hotmail.com>
2020-09-06 20:38:57 -0300

    adicionando interface, variavel e função ao componente TeacherItem

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,32 +1,50 @@
 import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem () {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name:string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    
+    function createNewConnection (){
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars1.githubusercontent.com/u/33336355?s=460&u=24da94177f2e62cb27fff42f57aa49395497a423&v=4" alt="Maria Fernanda"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong> Maria Fernanda</strong>
-                    <span>Química</span>
+                    <strong> {teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p> Entusiasta das melhores tecnologias de química avançada. 
-                <br /> <br />
-                Apaixonada por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora:
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Icone WhatsApp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
     </article>
     )
